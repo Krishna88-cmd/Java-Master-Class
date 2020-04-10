66ef5d3062e54fc7220a5a43d5a64388ed6060b4
krishna Neupane <krish_sanu67@yahoo.com>
2020-04-10 19:37:25 -0400

    this is my second time

--- PrimitiveType/OperatorsOperandsExpression.java
+++ PrimitiveType/OperatorsOperandsExpression.java
@@ -1,33 +1,54 @@
 package PrimitiveType;
 
-public class OperatorsOperandsExpression {
+public class OperatorsOperandsExpression<isCar> {
     public static void main(String[] args) {
 
 
-        int result = 1 + 3; //1+3=4
+        int result=1 + 3; //1+3=4
         System.out.println("1+3 = " + result);
-        int previousResult = result;
+        int previousResult=result;
         System.out.println("previous result = " + previousResult);
-        result = result -1;
+        result=result - 1;
         System.out.println("3-1 = " + result);
-        System.out.println("Previous Result = " +previousResult);
+        System.out.println("Previous Result = " + previousResult);
 
-        result = result * 10; // 2* 10 = 20
-        System.out.println("2 * 10 = " +result);
+        result=result * 10; // 2* 10 = 20
+        System.out.println("2 * 10 = " + result);
 
-        result =  result /5;  // 20/5
+        result=result / 5;  // 20/5
         System.out.println(" 20/5 = " + result);
 
-        result = result % 3 ; // the reminder of 4 %3
+        result=result % 3; // the reminder of 4 %3
         System.out.println("4 % 3 = " + result);
         // result = result +1
         result++;
-        System.out.println("1+1 = " +result);
-        result --;
-        System.out.println(" 1 - 1 = " +result );
+        System.out.println("1+1 = " + result);
+        result--;
+        System.out.println(" 1 - 1 = " + result);
 
         // result = result + 2;
-        result += 2; // 1+2 1
-        System.out.println("1 + 2 =" +result);
+        result+=2; // 1+2 1
+        System.out.println("1 + 2 =" + result);
+        boolean asAlien=false;
+        if (asAlien == false) {
+
+            System.out.println("it was not alien!");
+            System.out.println("i'm scared of alien!");
+
+        }
+        int topScore=100;
+        if (topScore > 80) {
+            System.out.println("You get the high score");
+        }
+        int secondTopScore=81;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Grater then second ");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) ;
+        System.out.println("either or both are true");
     }
+
+
 }
+
