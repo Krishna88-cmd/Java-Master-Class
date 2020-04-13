60a3be87cea1f34845c51f8b747d7f8bc7a223b1
krishna Neupane <krish_sanu67@yahoo.com>
2020-04-13 06:21:54 -0400

    code change

--- SecondAndMinutesChllenges/Main.java
+++ SecondAndMinutesChllenges/Main.java
@@ -2,27 +2,29 @@ package SecondAndMinutesChllenges;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getDurationString(65,45));
+        System.out.println(getDurationString(65, 45));
         System.out.println(getDurationString(3945));
 
     }
-    private static String getDurationString(long minutes ,long second){
-        if (( minutes < 0)  || (second < 0) || (second >59)) {
-            return "invalid value" ;
+
+    private static String getDurationString(long minutes, long second) {
+        if ((minutes < 0) || (second < 0) || (second > 59)) {
+            return "invalid value";
         }
-        long hours = minutes / 60;
-        long remainingMunites = minutes % 60;
-        return  hours + "h" + remainingMunites + "m" + second + "s";
+        long hours=minutes / 60;
+        long remainingMunites=minutes % 60;
+        return hours + "h" + remainingMunites + "m" + second + "s";
 
 
     }
-    private static String getDurationString(long seconds){
-        if ( seconds <0){
+
+    private static String getDurationString(long seconds) {
+        if (seconds < 0) {
             return "invalid value";
         }
-        long minutes = seconds /60;
-        long remainingSecond = seconds % 60;
-        return getDurationString(minutes,seconds);
+        long minutes=seconds / 60;
+        long remainingSecond=seconds % 60;
+        return getDurationString(minutes, seconds);
     }
 
 
