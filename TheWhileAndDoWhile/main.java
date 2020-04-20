6d3cd01bfc952282505eb1200fcf4f6e690f007d
krishna Neupane <krish_sanu67@yahoo.com>
2020-04-20 07:29:24 -0400

    add challange question

---
+++ TheWhileAndDoWhile/main.java
@@ -0,0 +1,18 @@
+package TheWhileAndDoWhile;
+
+public class main {
+    public static void main(String[] args) {
+        int count=0;
+        while (count != 5) {
+            System.out.println("count value is " + count);
+            count++;
+        }
+        while ((true)) {
+            if(count == 6){
+                break;
+        }
+            System.out.println("count value is " +count);
+            count ++;
+        }
+    }
+}
